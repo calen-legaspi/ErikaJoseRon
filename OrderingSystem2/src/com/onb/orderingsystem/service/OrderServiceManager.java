948d4e82e34ed1bcba02f42aa362b0552027d8cf
Erika Thea <erika.orangeandbronze.com>
2011-06-13 13:44:54 +0800

    updated order service manager
    Signed-off-by: Erika Thea <erika.orangeandbronze.com>

--- OrderingSystem2/src/com/onb/orderingsystem/service/OrderServiceManager.java
+++ OrderingSystem2/src/com/onb/orderingsystem/service/OrderServiceManager.java
@@ -1,7 +1,10 @@
 package com.onb.orderingsystem.service;
 
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashSet;
 
+import com.onb.orderingsystem.bean.OrderObject;
 import com.onb.orderingsystem.dao.DAOFactory;
 import com.onb.orderingsystem.dao.OrderDAO;
 import com.onb.orderingsystem.domain.Order;
@@ -39,5 +42,27 @@ public class OrderServiceManager {
 			e.printStackTrace();
 		}
 	}
+	
+	public Collection<OrderObject> findCustomerOrders(int customerId){
+		OrderDAO orderDao = dao.getOrderDAO();
+		Collection<OrderObject> orders = new HashSet<OrderObject>(); 
+		try {
+			for(Order order: orderDao.findAllOrderByCustomer(customerId)){
+				OrderObject orderBean = new OrderObject();
+				orderBean.setCustomerID(customerId);
+				orderBean.setDate(order.getDate());
+				orderBean.setOrders(order.getOrders());
+				orderBean.setTotal(order.getTotal());
+				orderBean.setStatus(order.getOrderStatus());
+				orderBean.setId(order.getId());
+				orders.add(orderBean);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return orders;
+	}
 
 }
+
