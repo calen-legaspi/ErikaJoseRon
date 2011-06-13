5cca4027d4d569f535398b3a1a2f487d9703eeee
Jose Araneta <jose.araneta@orangeandbronze.com>
2011-06-13 15:57:51 +0800

    edited the validation in OrderServlet doPost

--- OrderingSystem2/src/com/onb/orderingsystem/servlet/OrderServlet.java
+++ OrderingSystem2/src/com/onb/orderingsystem/servlet/OrderServlet.java
@@ -55,15 +55,17 @@ public class OrderServlet extends HttpServlet {
 		while(true){//make this less dumb
 			String str = productstr+Integer.toString(itemIndex);
 			skuNumber = request.getParameter(str);
-			if (skuNumber == null) break;//itemIndex is greater than the number of OrderItems
+			if (skuNumber == null) 
+				break;//itemIndex is greater than the number of OrderItems
 			str = quantitystr+Integer.toString(itemIndex);
 			str = request.getParameter(str);
-			if (str == null) throw new IllegalArgumentException("Invalid quantity");
+			if (str == null||str.equals("")) throw new IllegalArgumentException("Invalid quantity");
 			quantity = 	Integer.parseInt(str);
 			OrderItem orderitem = new OrderItem(OrderItemID++,productmanager.findProductBySKU(skuNumber), quantity);
 			order.add(orderitem);
 		}
 		ordermanager.insertOrder(order);
+		//update inventory here. iterate over order.getOrders() calling InventoryItemService updateQuantity() method
 	}
 
 }
