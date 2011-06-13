b0daebc58488c22403986e798e17ef48ba7aac29
Erika Thea <erika.orangeandbronze.com>
2011-06-13 09:46:15 +0800

    added setters & getters
    Signed-off-by: Erika Thea <erika.orangeandbronze.com>

--- OrderingSystem2/src/com/onb/orderingsystem/bean/CustomerObject.java
+++ OrderingSystem2/src/com/onb/orderingsystem/bean/CustomerObject.java
@@ -1,6 +1,7 @@
 package com.onb.orderingsystem.bean;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 public class CustomerObject implements Serializable{
 	
@@ -8,6 +9,9 @@ public class CustomerObject implements Serializable{
 	
 	private int id;
 	private String name;
+	private BigDecimal creditLimit;
+	private BigDecimal unpaidAmt;
+	private BigDecimal paidAmt;
 	
 	public CustomerObject() {}
 	
@@ -23,5 +27,22 @@ public class CustomerObject implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+	public BigDecimal getCreditLimit(){
+		return creditLimit;
+	}
+	public void setCreditLimit(BigDecimal creditLimit){
+		this.creditLimit = creditLimit;
+	}
+	public BigDecimal getUnpaidAmt(){
+		return unpaidAmt;
+	}
+	public void setUnpaidAmt(BigDecimal unpaidAmt){
+		this.unpaidAmt = unpaidAmt;
+	}
+	public BigDecimal getPaidAmt(){
+		return paidAmt;
+	}
+	public void setPaidAmt(BigDecimal paidAmt){
+		this.paidAmt = paidAmt;
+	}
 }
